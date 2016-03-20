33f7c6776c41141092af4f69e402ba31b1424a62
Pontus Persson <pom@spotify.com>
2016-03-20 14:17:04 +0100

    Tweak build.

--- build.js
+++ build.js
@@ -1,5 +1,13 @@
 'use strict';
 
+function mkdir(path) {
+  try {
+    fs.accessSync(path, fs.F_OK);
+  } catch (e) {
+    fs.mkdirSync(path);
+  }
+}
+
 let fs = require('fs');
 let htmlMinify = require('html-minifier').minify;
 
@@ -10,8 +18,13 @@ let html = fs.readFileSync(SRC_DIR + '/prod.html', 'utf8');
 let css = fs.readFileSync(SRC_DIR + '/css/pong.css', 'utf8');
 let js = fs.readFileSync(SRC_DIR + '/scripts/pong.js');
 
+let server = fs.readFileSync(SRC_DIR + '/scripts/server.js');
+
 html = html.replace('{{CSS}}', css);
 html = html.replace('{{JS}}', js);
+
+mkdir(BUILD_DIR);
+mkdir(BUILD_DIR + '/public');
 
 html = htmlMinify(html, {
   collapseWhitespace: true,
@@ -19,10 +32,5 @@ html = htmlMinify(html, {
   minifyJS: true,
 });
 
-try {
-  fs.accessSync(BUILD_DIR, fs.F_OK);
-} catch (e) {
-  fs.mkdirSync(BUILD_DIR);
-}
-
-fs.writeFileSync(BUILD_DIR + '/index.html', html);
+fs.writeFileSync(BUILD_DIR + '/server.js', server);
+fs.writeFileSync(BUILD_DIR + '/public/index.html', html);
